71663e3d34ca1427558865364ac8dce6df41448b
nyawai <138332371+nyanwai@users.noreply.github.com>
2024-06-10 02:16:41 +0200

    added view counter by counter.dev

--- script.js
+++ script.js
@@ -37,3 +37,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
     animateText();
 });
+
+// This function updates the view counter by getting the count from a specific element added by the Counter.dev script
+function updateViewCounter() {
+    const counterElement = document.querySelector('[data-id="fd011bcf-b669-4d42-9fd9-b82353653704"]');
+    if (counterElement) {
+        document.getElementById('view-counter').innerText = counterElement.innerText;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', updateViewCounter);
+
+
+
+
+
